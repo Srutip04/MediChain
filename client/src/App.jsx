0c4797e6ebc8b76c4719939b3b33d3722543bfc2
Srutip04 <subuhishruti75@gmail.com>
2024-02-15 07:24:24 +0530

    Update App.jsx and EthProvider.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,71 @@
-import { EthProvider } from "./contexts/EthContext";
+// import { EthProvider } from "./contexts/EthContext";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import SupplyChain from "./contracts/SupplyChain.json";
 import LandingPage from "./components/Home/LandingPage";
 import { Routes,Route } from "react-router-dom";
 function App() {
+  const [account, setAccount] = useState(null);
+  const [supplyChain, setSupplyChain] = useState(null);
+  const [identicon, setIdenticon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [web3, setWeb3] = useState(null);
+
+  useEffect(() => {
+    const loadWeb3 = async () => {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        window.alert(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+      }
+      setWeb3(window.web3);
+    };
+
+    loadWeb3();
+  }, []);
+
+  useEffect(() => {
+    const loadBlockChain = async () => {
+      if (web3) {
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts);
+        setAccount(accounts[0]);
+        const networkId = await web3.eth.net.getId();
+        const networkData = SupplyChain.networks[networkId];
+        if (networkData) {
+          const supplyChainInstance = new web3.eth.Contract(
+            SupplyChain.abi,
+            networkData.address
+          );
+          setSupplyChain(supplyChainInstance);
+          setLoading(false);
+          console.log(supplyChainInstance);
+        } else {
+          window.alert(
+            "Supply chain contract not deployed to detected network."
+          );
+        }
+      }
+    };
+
+    loadBlockChain();
+  }, [web3]);
+
   return (
-    <EthProvider>
+    // <EthProvider>
+     loading ? <div>Loding</div> :
       <div id="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<LandingPage/>}/>
         </Routes>         
       </div>
-    </EthProvider>
+    // </EthProvider>
   );
 }
 
